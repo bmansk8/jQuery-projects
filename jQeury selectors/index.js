9e15d202f14b12ba1727cb81d6a6de2fd09ef0ee
bmansk8 <bmansk8@yahoo.com>
2019-10-21 12:13:22 -0400

    query selector exersice
    completed a query selector exercise using jQuery.

---
+++ jQeury selectors/index.js
@@ -0,0 +1,14 @@
+var styles = {
+    color: 'red',
+    border:'2px solid black'
+}
+
+$('h1').css(styles);
+
+$('a').css('font-size', '30px')
+
+$('li').css({
+    fontSize:'15px',
+    border:'3px dashed purple',
+    background:'rgba(89,45,20,0.5)'
+})
---
+++ selectors challenge/index.js
@@ -0,0 +1,16 @@
+$("div").css("background", "purple");
+$(".highlight").css("width", "200px");
+$("#third").css("border", "orange 2px solid");
+$("div").first().css("color", "pink");
+
+/*
+include jQuery ✓
+
+select all divs and give them pruple background ✓
+
+select div with highlight and make 200px wide ✓
+
+select div with id third and give it orange border ✓
+
+bonus: select fist div make text pink ✓
+*/
